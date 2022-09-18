ba31881a1b64e2f73dfb1f44236cd87df9d1332e
asmachebbi97 <asmachebbi222@gmail.com>
2022-09-18 21:50:45 +0200

    asma

---
+++ src/main/java/com/courzelo/quiz_skills/skills/businesses/iservices/IServiceSkills.java
@@ -0,0 +1,18 @@
+package com.courzelo.quiz_skills.skills.businesses.iservices;
+
+
+import com.courzelo.quiz_skills.skills.entities.Microskills;
+import com.courzelo.quiz_skills.skills.entities.Type;
+import com.courzelo.quiz_skills.skills.entities.dtos.MacroskillsDTO;
+import com.courzelo.quiz_skills.skills.entities.dtos.MicroskillsDTO;
+import com.courzelo.quiz_skills.skills.entities.dtos.SkillsDTO;
+
+import java.util.List;
+
+public interface IServiceSkills {
+
+    public SkillsDTO getskillbyid(String id);
+    public List<MacroskillsDTO> getmacroskills(String id, Type type);
+    public List<MicroskillsDTO> getmicroskills(String iduser, String name, Type type);
+    public List<List<Microskills>> getallmicroskills(String iduser, Type type);
+}
